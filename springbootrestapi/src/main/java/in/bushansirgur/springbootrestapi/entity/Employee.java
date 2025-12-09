43638219f7d33d94bc4c7193aa25ee8904d6d64b
Dheeraj Kumar <dheerajkumar86017@gmail.com>
2025-12-09 08:01:22 +0530

    first commit

--- springbootrestapi/src/main/java/in/bushansirgur/springbootrestapi/entity/Employee.java
+++ springbootrestapi/src/main/java/in/bushansirgur/springbootrestapi/entity/Employee.java
@@ -1,29 +1,24 @@
 package in.bushansirgur.springbootrestapi.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import lombok.*;
 
 @Entity
-@Table(name = "tbl_employees")
-@Data
-@AllArgsConstructor
+@Table(name = "employees")
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Employee {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	private String name;
-	
-	private String location;
-	
-	private String department;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String location;
+
+    private String department;
 }
