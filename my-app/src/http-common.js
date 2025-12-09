b9bd8da67422daf382a7fd610ff468ab97303433
Dheeraj Kumar <dheerajkumar86017@gmail.com>
2025-12-09 10:01:58 +0530

    hello

--- my-app/src/http-common.js
+++ my-app/src/http-common.js
@@ -1,7 +1,9 @@
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api/v1';
+
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:8080/api/v1',
+    baseURL: API_URL,
     headers: {
         'Content-Type': 'application/json'
     }
